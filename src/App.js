575ddd7345f57ca996fe11d22603f9b44db11ea2
s-p737 <stuti.p.737@gmail.com>
2025-12-28 13:30:05 -0600

    Add gitignore

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState('');
+  
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=64dcf1c2a66a83b6086ceb80a374885b`
+
+  const searchLocation = (event) => {
+    if (event.key === 'Enter') {
+      axios.get(url).then((response) => {
+        setData(response.data);
+        console.log(response.data);
+    })
+    setLocation('');
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      
+      <div className="search"> 
+        <input 
+          value={location}
+          onChange={event => setLocation(event.target.value)}
+          onKeyDown={searchLocation}
+          placeholder='Enter Location'
+          type="text"/>
+      </div>
+
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>{data.name}</p>
+          </div>
+
+          <div className="temp">
+            <h1>{data.main ? `${Math.round((data.main.temp - 273.15)*(9/5)+32)}°F` : null} </h1>
+          </div>
+
+          <div className="description">
+            {data.weather ? data.weather[0].description : null}
+          </div>
+        </div>
+
+        <div className="bottom">
+          <div className="feels">
+            {data.main ? <p className='bold'>{Math.round((data.main.feels_like - 273.15)*(9/5)+32)}°F</p> : null}
+            <p>Feels Like</p>
+          </div>
+
+          <div className="humidity">
+            {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
+            <p>Humidity</p>
+          </div>
+
+          <div className="wind">
+            {data.wind ? <p className='bold'>{data.wind.speed} MPH</p> : null}
+            <p>Wind Speed</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
